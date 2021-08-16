2dfaa81d99a648927bbbd8766b84cca5a6f7ef36
otegami <a.s.takuya1026@gmail.com>
2021-08-16 03:39:41 -0700

    5.2.4.1 relations like has_many

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const { ApolloServer } = require(`apollo-server`)
 
 // フィールド
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -16,6 +23,7 @@ const typeDefs = `
     name: String!
     description: String!
     category: PhotoCategory!
+    postedBy: User!
   }
 
   input PostPhotoInput {
@@ -36,7 +44,35 @@ const typeDefs = `
 
 // リゾルバ
 let _id = 0
-let photos = []
+
+let users = [
+  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+  { "githubLogin": "gPlake", "name": "Glen Plake" },
+  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+
+let photos = [
+  {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes",
+    "category": "ACTION",
+    "githubUser": "gPlake"
+  },
+  {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+  },
+  {
+    "id": "3",
+    "name": "Gunbarrel 25",
+    "description": "25 laps on gunbarrel today",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+  } 
+]
 
 const resolvers = {
   Query: {
@@ -53,7 +89,15 @@ const resolvers = {
   },
   // トリビアルリゾルバ
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    }
   }
 }
 
