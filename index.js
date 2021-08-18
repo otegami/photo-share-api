91e21026c0cdcacb19badad67c7b9575c714afee
otegami <a.s.takuya1026@gmail.com>
2021-08-18 05:46:41 -0700

    5.2.4.2 Many to many

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -24,6 +25,7 @@ const typeDefs = `
     description: String!
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -74,6 +76,13 @@ let photos = [
   } 
 ]
 
+let tags = [
+  { "photoID": "1", "userID": "gPlake" },
+  { "photoID": "2", "userID": "sSchmidt" },
+  { "photoID": "2", "userID": "mHattrup" },
+  { "photoID": "2", "userID": "gPlake" }
+]
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -87,17 +96,24 @@ const resolvers = {
       return newPhoto
     }
   },
-  // トリビアルリゾルバ
   Photo: {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
-    }
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
