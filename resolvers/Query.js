0b6e6acbea9f733cd23673cdc11c3dd0c8733ef0
otegami <a.s.takuya1026@gmail.com>
2021-09-09 20:58:53 -0700

    FIX 関数で値を return する形に修正

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,23 +1,5 @@
-export const me = (parent, args, { currentUser }) => { currentUser }
-
-export const totalPhotos = (parent, args, { db }) => {
-  db.collection(`photos`)
-    .estimatedDocumentCount()
-}
-
-export const allPhotos = (parent, args, { db }) => {
-  db.collection(`photos`)
-    .find()
-    .toArray()
-}
-
-export const totalUsers = (parent, args, { db }) => {
-  db.collection(`users`)
-    .estimatedDocumentCount()
-}
-
-export const allUsers = (parent, args, { db }) => {
-  db.collection(`users`)
-    .find()
-    .toArray()
-}
+export const me = (parent, args, { currentUser }) => currentUser
+export const totalPhotos = (parent, args, { db }) => db.collection('photos').estimatedDocumentCount()
+export const allPhotos = (parent, args, { db }) => db.collection('photos').find().toArray()
+export const totalUsers = (parent, args, { db }) => db.collection('users').estimatedDocumentCount()
+export const allUsers = (parent, args, { db }) => db.collection('users').find().toArray()
